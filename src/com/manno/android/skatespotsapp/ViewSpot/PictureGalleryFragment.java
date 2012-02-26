7cc2d04671aee937184dd7f0c7f6846cb30ccd2b
Jason Manning <jasonmanning23@gmail.com>
2012-02-26 22:51:16 +0800

    First Commit

---
+++ src/com/manno/android/skatespotsapp/ViewSpot/PictureGalleryFragment.java
@@ -0,0 +1,9 @@
+package com.manno.android.skatespotsapp.ViewSpot;
+
+/**
+ * User: jason
+ * Date: 19/02/12
+ * Time: 12:22 PM
+ */
+public class PictureGalleryFragment {
+}
---
+++ src/com/manno/android/skatespotsapp/ViewSpot/SpotInfoFragment.java
@@ -0,0 +1,9 @@
+package com.manno.android.skatespotsapp.ViewSpot;
+
+/**
+ * User: jason
+ * Date: 17/02/12
+ * Time: 3:13 PM
+ */
+public class SpotInfoFragment {
+}
